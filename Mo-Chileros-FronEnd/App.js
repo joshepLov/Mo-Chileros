e1555929f6ecdf81681000299e273b82ff390617
mhurtado2021407 <mhurtado-2021407@kinal.edu.gt>
2023-06-05 01:28:34 -0600

    Changes FrontEnd: Created Navigate(router) log in, need fix bug axios

--- Mo-Chileros-FronEnd/App.js
+++ Mo-Chileros-FronEnd/App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { LogInScreen } from './src/screens/LogInScreen';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import { Navigation } from './Navigation';
 
 export default function App() {
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  <Navigation></Navigation>
+    
   );
 }
 
